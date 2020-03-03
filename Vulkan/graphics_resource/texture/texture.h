20b0efb14a0567585dbc2814efb77e991a618235
kato_shogo <kato_shogo@ilca.co.jp>
2020-03-03 17:32:12 +0900

    texture桂移動

---
+++ Vulkan/graphics_resource/texture/texture.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stb_image.h>
+
+namespace vengine
+{
+    class VulkanGraphicsBackend;
+
+    namespace texture {
+        typedef struct PixelData {
+            stbi_uc* pixels_;
+            int width_;
+            int height_;
+            int channels_;
+        } PixelData;
+    } // texture
+
+    class Texture final
+    {
+    private:
+        std::shared_ptr<VulkanGraphicsBackend> graphics_backend_;
+        VkImage image_;
+        VkDeviceMemory memory_;
+        VkImageView image_view_;
+        int mip_levels_;
+    public:
+        Texture(std::shared_ptr<VulkanGraphicsBackend> const graphics_backend, const texture::PixelData& pixel_data);
+        ~Texture();
+
+        void create(const texture::PixelData& pixel_data);
+        void createTextureSampler();
+    };
+} // vengine
---
+++ Vulkan/graphics_resource/texture/texture_factory.cpp
@@ -0,0 +1,12 @@
+#include "texture_builder.h"
+
+namespace rengine
+{
+    TextureFactory::TextureBuilder()
+    {
+    }
+
+    TextureFactory::~TextureFactory()
+    {
+    }
+} // rengine
